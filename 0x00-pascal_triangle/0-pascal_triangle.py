bd57b9bf2ee206c1dfb4f1509e646e9f94b4e3da
Mohammed Amer <loard.amer.2010@gmail.com>
2024-10-04 03:58:34 +0300

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,4 +3,30 @@
 
 
 def pascal_triangle(n):
-    pass
+    ''' function to represent pascal triangle
+    n (int):
+        the number of levels for triangle
+    Return:
+        list of list represents pascal triangle
+    '''
+    if n <= 0:
+        return []
+    else:
+        topList = []
+        for i in range(1, n + 1):
+            levelList = []
+            if i == 1:
+                levelList.append(1)
+                topList.append(levelList)
+            elif i == 2:
+                levelList.append(1)
+                levelList.append(1)
+                topList.append(levelList)
+            else:
+                levelList.append(1)
+                for j in range(i - 2):
+                    levelList.append(topList[i - 2][j] +
+                                     topList[i - 2][j + 1])
+                levelList.append(1)
+                topList.append(levelList)
+        return topList
