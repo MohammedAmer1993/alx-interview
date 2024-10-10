b80233223b4f7624cdc4d5659b741c055d20c9aa
Mohammed Amer <loard.amer.2010@gmail.com>
2024-10-10 18:55:23 +0300

    interview problem dynamic programming

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' this is a function to solve dynamic programming problem the
+problem is we have boxes of boxes and we need to know if we have every key for a box'''
+
+
+def canUnlockAll(boxes):
+    array = [0]
+
+    def recfunc(count):
+        for i in boxes[count]:
+            if i in array:
+                return
+            else:
+                array.append(i)
+                recfunc(i)
+    recfunc(0)
+    if len(array) == len(boxes):
+        return True
+    else:
+        return False
