ce87d6539b620fa38d9108d24ce016b1fad76703
Mohammed Amer <loard.amer.2010@gmail.com>
2024-10-10 19:11:46 +0300

    interview problem dynamic programming

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,11 +5,21 @@ if we have every key for a box'''
 
 
 def canUnlockAll(boxes):
+    '''function to check for keys of the boxes
+    para:
+      boxes (list of list of int)
+
+    return:
+        True: if we have all the keys
+        False: if we miss some keys
+    '''
     array = [0]
-    if len(boxes) == 0:
-        return False
 
     def recfunc(count):
+        ''' function to do recursive search
+        para:
+            count: the box we are inside right now
+        '''
         for i in boxes[count]:
             if i in array:
                 return
