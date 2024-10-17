00c590450b3a0e77c1a626edb1c64336d43c6d36
Mohammed Amer <loard.amer.2010@gmail.com>
2024-10-17 19:18:29 +0300

    minimum operation interview problem

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-''' this is a module for finding minimum number of
- operation to copy a content of a file reapeatdly'''
+''' this is a module for finding minimum number of operation to copy a content of a file reapeatdly'''
 
 
 def minOperations(n):
@@ -10,14 +9,21 @@ def minOperations(n):
     '''
     if n <= 0:
         return 0
-    primeNum = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541
-
-                ]
+    if not isinstance(n, int):
+        return 0
+    primeNum = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                31, 37, 41, 43, 47, 53, 59, 61, 67,
+                71, 73, 79, 83, 89, 97, 101, 103,
+                107, 109, 113, 127, 131, 137, 139,
+                149, 151, 157, 163, 167, 173, 179,
+                181, 191, 193, 197, 199, 211, 223,
+                227, 229, 233, 239, 241, 251, 257,
+                263, 269, 271, 277, 281, 283, 293]
     numberOfOper = 0
     if n == 1:
         return 0
     for i in primeNum:
         if n % i == 0:
-            numberOfOper = i + minOperations(n / i)
+            numberOfOper = i + minOperations(int(n / i))
             break
     return numberOfOper
