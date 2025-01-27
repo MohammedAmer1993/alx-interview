069711a6ea95eff23245b67bb6534f143b457984
Mohammed Amer <loard.amer.2010@gmail.com>
2025-01-27 08:27:28 +0200

    prime number

--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -2,5 +2,8 @@
 
 isWinner = __import__('0-prime_game').isWinner
 
+nums = [0] * 10000
+for i in range(10000):
+    nums[i] = i
 
-print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+print("Winner: {}".format(isWinner(10000, nums)))
